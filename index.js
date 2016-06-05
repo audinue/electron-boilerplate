b4805937e7a874af81f355aa3ad6eded4859206e
Audi Nugraha <audinue@gmail.com>
2016-06-06 05:21:58 +0700

    Update index.js to follow StandardJS

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
-var app           = require('electron').app;
-var BrowserWindow = require('electron').BrowserWindow;
+var app = require('electron').app
+var BrowserWindow = require('electron').BrowserWindow
+var path = require('path')
 
-app.on('ready', function() {
-	var window = new BrowserWindow({
-		width: 600,
-		height: 400,
-		title: 'Hello World'
-	});
-	window.on('close', function() {
-		app.quit(0);
-	});
-	window.loadURL('file:///' + __dirname + '/index.html');
-});
+app.on('ready', function () {
+  var window = new BrowserWindow({
+    width: 600,
+    height: 400,
+    title: 'Hello World'
+  })
+  window.on('close', function () {
+    app.quit(0)
+  })
+  window.loadURL(path.resolve('./index.html'))
+})
